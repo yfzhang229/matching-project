6ab79a3748a287bed4f6bcd6b85646a12d32493f
Yufeng Zhang <zhangyufeng@zhangyufengs-MacBook-Pro.local>
2020-06-12 11:49:35 -0500

    Project uploaded

---
+++ match_tools.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 01:12:12 2020
+
+@author: zhangyufeng
+"""
+
+import os
+
+def get_file_paths(directory='graphs'):
+    file_paths = []
+    for directory, _, files in os.walk(directory):
+        for file_name in files:
+            if file_name.endswith('.mtx'):
+                file_path = os.path.join(directory, file_name)
+                file_paths.append(file_path)
+    return file_paths
+
+def from_mtx(mtx):
+    '''Construct a graph from the mtx, the output G is a dictionary, 
+    iter(G) is the set of vertices, iter(G[v]) is the neighborhood of v in G'''
+    G = {}
+    num_vertex = max(mtx.shape[0], mtx.shape[1])
+    for v in range(num_vertex):
+        G[v] = set([])
+    
+    num_edge = mtx.nnz
+    for k in range(num_edge):
+        if mtx.row[k] != mtx.col[k]:
+            G[mtx.row[k]].add(mtx.col[k])
+            G[mtx.col[k]].add(mtx.row[k])
+    return G
+
+class union_find:
+    '''Union Find structure'''
+    def __init__(self):
+        self.parents = {}
+        self.weights = {}
+        
+    def __getitem__(self, object):
+        if object not in self.parents:
+            self.parents[object] = object
+            self.weights[object] = 1
+            return object
+        
+        path = [object]
+        root = self.parents[object]
+        while root != path[-1]:
+            path.append(root)
+            root = self.parents[root]
+            
+        for v in path:
+            self.parents[v] = root
+        
+        return root
+    
+    def select(self, base):
+        root = self[base]
+        for v in self.parents:
+            if self[v] == root:
+                self.parents[v] = base
+                
+    topless = -1
+    def union(self, *objects, base=-1):
+        if base == -1:
+            roots = [self[x] for x in objects]
+            root = max([(self.weights[r], r) for r in roots])[1]
+            for r in roots:
+                if r != root:
+                    self.parents[r] = root
+                    self.weights[root] += self.weights[r]
+        else:
+            roots = [self[x] for x in objects]
+            root = base
+            for r in roots:
+                if r != root:
+                    self.parents[r] = root
+                    self.weights[root] += self.weights[r]
---
+++ matching.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 01:20:04 2020
+
+@author: zhangyufeng
+"""
+
+from match_tools import union_find, from_mtx
+from scipy.io import mmread
+
+
+global EVEN, ODD, NULL
+EVEN = 0
+ODD = 1
+NULL = -1
+
+def greedy_matching(G):
+    '''Get a greedy matching'''
+    M = {}
+    for v in G:
+        if v not in M:
+            for u in G[v]:
+                if u not in G[v]:
+                    M[u] = v
+                    M[v] = u
+                    break
+    return M
+
+def card_matching(mate):
+    count = 0
+    for v in mate:
+        if mate[v] != NULL:
+            count += 1
+        
+    return count / 2
+
+def is_matching(G, mate):
+    for v in mate:
+        if mate[v] != NULL:
+            w = mate[v]
+            if mate[w] != v:
+                return False
+            elif w not in G[v]:
+                return False
+    return True
+
+def find_max_matching(G):
+    
+    M = greedy_matching(G)
+    
+    
+    mate = {}
+    for v in G:
+        if v in M:
+            mate[v] = M[v]
+        else:
+            mate[v] = NULL
+            
+    card = card_matching(mate)
+                
+            
+    def alternating_forest():
+        
+        label = {}
+        pred = {}
+        pnode = union_find()  
+        bridge = {}
+        
+        
+        def alternating_tree(v):
+            
+            Q = [v]
+            label[v] = EVEN
+            
+            def examine(v, w):
+                
+                def extend_tree(v, w):
+                    
+                    label[w] = ODD
+                    pred[w] = v
+                    label[mate[w]] = EVEN
+                    pred[mate[w]] = w
+                    Q.append(mate[w])
+                
+                def shrink_blossom(v, w):
+                    
+                    def find_base(v, w):
+                    
+                        path1 = {}
+                        path2 = {}
+                        head1 = v
+                        head2 = w
+                        def step(path, head):
+                            head = pnode[head]
+                            if pred[head] == -1:
+                                return head
+                            parent = pnode[pred[head]]
+                            path[head] = parent
+                            path[parent] = pnode[pred[parent]]
+                            return path[parent]
+                            
+                            
+                        while 1:
+                            head1 = step(path1, head1)
+                            head2 = step(path2, head2)
+                            if head1 == head2:
+                                return head1
+                            elif head1 in path2:
+                                return head1
+                            elif head2 in path1:
+                                return head2
+                            
+                    def find_shrink_path(b, v, w):
+                        path = [pnode[v]]
+                        bri = (v, w)
+                        while path[-1] != b:
+                            u = pred[path[-1]]
+                            path.append(u)
+                            bridge[u] = bri
+                            Q.append(u)
+                            path.append(pnode[pred[u]])
+                        return path
+                            
+                    
+                    b = pnode[find_base(v, w)]
+                    path1 = find_shrink_path(b, v, w)
+                    path2 = find_shrink_path(b, w, v)
+                    pnode.union(*path1, base=b)
+                    pnode.union(*path2, base=b)
+                    
+                def augmenting_path(v, w):
+                    
+                    topless = object()
+                    def find_path(s, t=topless):
+                        path = []
+                        while 1:
+                            while label[s] == ODD:
+                                v, w = bridge[s]
+                                vs = find_path(v, s)
+                                vs.reverse()
+                                path += vs
+                                s = w
+                            path.append(s)
+                            if mate[s] == NULL:
+                                return path
+                            u = mate[s]
+                            path.append(u)
+                            if u == t:
+                                return path
+                            s = pred[u]
+                        
+#                        if s == t:
+#                            return [s]
+#                        
+#                        elif label[s] == EVEN:
+#                            return [s, mate[s]] + find_path(pred[mate[s]], t)
+#                        else:
+#                            v, w = bridge[s]
+#                            vs = find_path(v, mate[s])
+#                            vs.reverse()
+#                            return [s] + vs + find_path(w, t)
+                        
+                    return [w] + find_path(v)
+                        
+                    
+                
+                
+                vb = pnode[v]
+                wb = pnode[w]
+                
+                if vb != wb:
+                    if label[wb] == NULL:
+                        if mate[w] == NULL:
+                           
+                            P = augmenting_path(v, w)  
+                           
+                            k = 0
+                            while k < len(P):
+                                u = P[k]
+                                v = P[k + 1]
+                                mate[u] = v
+                                mate[v] = u
+                                k += 2
+                                
+                            
+                            return True
+                            
+                        else:
+                            
+                            extend_tree(v, w)
+                            
+                            return False
+                            
+                    elif label[wb] == EVEN:
+                        
+                        shrink_blossom(v, w)
+                       
+                        return False
+                
+                        
+                        
+                        
+            
+            current = 0
+            while current < len(Q):
+                
+                v = Q[current]
+                current += 1
+                for w in G[v]:
+                    if examine(v, w):
+                        return True
+            return False
+        
+        
+        
+        for v in G:
+            label[v] = NULL
+            pred[v] = NULL
+            pnode[v]
+            
+        for v in G:
+            if mate[v] == NULL:
+                if alternating_tree(v):
+                    return True
+        
+        return False
+    
+    while alternating_forest():
+        card = card_matching(mate)
+        #if card % 200 == 0:
+            #print(f'card of mate is {card_matching(mate)}')
+        pass
+    
+    return mate
+
+
+
+# test
+
+if __name__ == "__main__":
+    
+    mtx = mmread('graphs/poli.mtx')
+#    mtx = mmread('graphs/poli.mtx')
+    G = from_mtx(mtx)
+    mate = find_max_matching(G)
+    print(f'The size of maximum cardinality matching is {card_matching(mate)}, is matching: {is_matching(G, mate)}')
+                
